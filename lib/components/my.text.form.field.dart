726436466ff43c54d33f58ee6da64c3b529bc848
adalbertojr-git <atajr@lsdi.ufma.br>
2022-11-19 19:36:09 -0300

    Ajustes na MyTextFormField

--- lib/components/my.text.form.field.dart
+++ lib/components/my.text.form.field.dart
@@ -43,7 +43,7 @@ class MyTextFormField extends StatelessWidget {
   Widget build(BuildContext context) {
     maskTextInputFormatter = maskTextInputFormatter ?? MaskTextInputFormatter();
     return Padding(
-      padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+      padding: const EdgeInsets.fromLTRB(10.0, 3.0, 10.0, 3.0),
       child: TextFormField(
         textCapitalization: TextCapitalization.words,
         obscureText: hidden ?? false,
