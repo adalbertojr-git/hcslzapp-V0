5ea08f10e36baa5fa7d79f38f3119ecbfa2222fe
adalbertojr-git <atajr@lsdi.ufma.br>
2022-12-02 10:01:48 -0300

    Ajustes no layout da partnership.add.page.dart

--- lib/pages/partnership/partnership.add.page.dart
+++ lib/pages/partnership/partnership.add.page.dart
@@ -1,194 +1,173 @@
 import 'dart:io';
+import 'package:asuka/snackbars/asuka_snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hcslzapp/common/labels.and.hints.dart';
 import 'package:hcslzapp/common/photo.image.provider.dart';
 import 'package:hcslzapp/components/button.dart';
-import 'package:hcslzapp/components/top.bar.dart';
 import 'package:hcslzapp/components/transaction.auth.dialog.dart';
 import 'package:hcslzapp/controllers/partnership.add.controller.dart';
 import 'package:hcslzapp/enums/associated.status.dart';
 import 'package:hcslzapp/components/my.text.form.field.dart';
 import 'package:hcslzapp/models/partnership.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
-import 'package:asuka/asuka.dart' as asuka;
+
+import '../../components/my.appbar.dart';
+import '../../components/my.bottom.appbar.dart';
 
 const String _pathNoImage = 'assets/imgs/noImage.png';
 const String _title = 'Parceria';
 
-class PartnershipAddPage extends StatefulWidget {
+class PartnershipAddPage extends StatelessWidget {
   final Partnership? partnership;
+  final PartnershipAddController _controller = PartnershipAddController();
 
   PartnershipAddPage(this.partnership);
 
   @override
-  _PartnershipAddPageState createState() => _PartnershipAddPageState();
-}
-
-class _PartnershipAddPageState extends State<PartnershipAddPage> {
-  PartnershipAddController _controller = PartnershipAddController();
-
-  @override
-  void initState() {
-    _controller.partnership = widget.partnership ?? _controller.partnership;
+  Widget build(BuildContext context) {
+    _controller.partnership = partnership ?? _controller.partnership;
     _controller.init();
-    super.initState();
+    return Scaffold(
+      appBar: MyAppBar(
+        partnership == null ? 'Adicionar ' + _title : 'Editar ' + _title,
+      ),
+      bottomNavigationBar: MyBottomAppBar(),
+      body: _widgets(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Button(
+        icon: Icons.save,
+        onClick: () => partnership == null ? _save(context) : _update(context),
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Container(
-          padding: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.white30, Colors.deepOrange],
-              begin: FractionalOffset.topLeft,
-              end: FractionalOffset.bottomRight,
-            ),
+  _widgets() => ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10.0,
           ),
-          height: MediaQuery.of(context).size.height,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                TopBar(
-                  title: widget.partnership == null
-                      ? 'Adicionar ' + _title
-                      : 'Editar ' + _title,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.add_photo_alternate,
+                  color: Colors.black,
+                  size: 28.0,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.add_photo_alternate,
-                        color: Colors.black,
-                        size: 28.0,
-                      ),
-                      onPressed: _controller.getImageFromGallery,
-                    ),
-                    _photo(),
-                    IconButton(
-                      icon: Icon(
-                        Icons.add_a_photo,
-                        color: Colors.black,
-                        size: 25.0,
-                      ),
-                      onPressed: _controller.getImageFromCamera,
-                    ),
-                  ],
+                onPressed: _controller.getImageFromGallery,
+              ),
+              _photo(),
+              IconButton(
+                icon: Icon(
+                  Icons.add_a_photo,
+                  color: Colors.black,
+                  size: 25.0,
                 ),
-                SizedBox(
-                  height: 5.0,
+                onPressed: _controller.getImageFromCamera,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Observer(
+            builder: (_) {
+              return MyTextFormField(
+                textEditingController: _controller.partnerCtrl,
+                label: labelPartner,
+                hint: hintPartner,
+                icon: Icons.emoji_people,
+                inputType: TextInputType.text,
+                onChanged: _controller.formController.changePartner,
+                errorText: _controller.validatePartner(),
+              );
+            },
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: <Widget>[
+              Expanded(
+                child: MyTextFormField(
+                  textEditingController: _controller.phone1Ctrl,
+                  label: labelPhone,
+                  hint: hintPhone,
+                  icon: Icons.phone,
+                  inputType: TextInputType.number,
+                  maskTextInputFormatter:
+                      MaskTextInputFormatter(mask: "(##) #####-####"),
                 ),
-                Observer(
-                  builder: (_) {
-                    return MyTextFormField(
-                      textEditingController: _controller.partnerCtrl,
-                      label: labelPartner,
-                      hint: hintPartner,
-                      icon: Icons.emoji_people,
-                      inputType: TextInputType.text,
-                      onChanged: _controller.formController.changePartner,
-                      errorText: _controller.validatePartner(),
-                    );
-                  },
+              ),
+              Expanded(
+                child: MyTextFormField(
+                  textEditingController: _controller.phone2Ctrl,
+                  label: labelPhone,
+                  hint: hintPhone,
+                  icon: Icons.phone,
+                  inputType: TextInputType.number,
+                  maskTextInputFormatter:
+                      MaskTextInputFormatter(mask: "(##) #####-####"),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: <Widget>[
-                    Expanded(
-                      child: MyTextFormField(
-                        textEditingController: _controller.phone1Ctrl,
-                        label: labelPhone,
-                        hint: hintPhone,
-                        icon: Icons.phone,
-                        inputType: TextInputType.number,
-                        maskTextInputFormatter:
-                            MaskTextInputFormatter(mask: "(##) #####-####"),
-                      ),
-                    ),
-                    Expanded(
-                      child: MyTextFormField(
-                        textEditingController: _controller.phone2Ctrl,
-                        label: labelPhone,
-                        hint: hintPhone,
-                        icon: Icons.phone,
-                        inputType: TextInputType.number,
-                        maskTextInputFormatter:
-                            MaskTextInputFormatter(mask: "(##) #####-####"),
-                      ),
-                    ),
-                  ],
-                ),
-                MyTextFormField(
-                  textEditingController: _controller.addressCtrl,
-                  label: labelAddress,
-                  hint: hintAddress,
-                  inputType: TextInputType.text,
-                  nLines: 2,
-                ),
-                MyTextFormField(
-                  textEditingController: _controller.promotionCtrl,
-                  label: labelPromotion,
-                  hint: hintPromotion,
-                  inputType: TextInputType.text,
-                  nLines: 4,
-                ),
-                widget.partnership == null
-                    ? Container()
-                    : Container(
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(0.0, 3.0, 2.0, 3.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.baseline,
-                            textBaseline: TextBaseline.alphabetic,
-                            children: <Widget>[
-                              Expanded(
-                                child: const Text(
-                                  'Status:',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  height: 55.0,
-                                  child: DropdownButtonFormField(
-                                    decoration: const InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ),
-                                    value: _controller.currentStatus,
-                                    items: getStatus(),
-                                    onChanged:
-                                        _controller.changedStatusDropDownItem,
-                                  ),
-                                ),
-                              ),
-                            ],
+              ),
+            ],
+          ),
+          MyTextFormField(
+            textEditingController: _controller.addressCtrl,
+            label: labelAddress,
+            hint: hintAddress,
+            inputType: TextInputType.text,
+            nLines: 2,
+          ),
+          MyTextFormField(
+            textEditingController: _controller.promotionCtrl,
+            label: labelPromotion,
+            hint: hintPromotion,
+            inputType: TextInputType.text,
+            nLines: 4,
+          ),
+          partnership == null
+              ? Container()
+              : Container(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 3.0, 2.0, 3.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Expanded(
+                          child: const Text(
+                            'Status:',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                SizedBox(
-                  height: 80.0,
+                        Expanded(
+                          child: Container(
+                            height: 55.0,
+                            child: DropdownButtonFormField(
+                              decoration: const InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              value: _controller.currentStatus,
+                              items: getStatus(),
+                              onChanged: _controller.changedStatusDropDownItem,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: Button(
-          icon: Icons.save,
-          onClick: () => widget.partnership == null ? _save() : _update(),
-        ),
+        ],
       );
 
   _photo() => Container(
@@ -222,45 +201,26 @@ class _PartnershipAddPageState extends State<PartnershipAddPage> {
                 ) as ImageProvider,
       fit: BoxFit.fill);
 
-  _save() async {
+  _save(BuildContext context) async {
     if (_controller.hasErrors) {
-      asuka.showSnackBar(
-        SnackBar(
-          content: const Text('Atenção: Existem erros no formulário que devem '
-              'ser corrigidos antes de efetivar a transação.'),
-        ),
-      );
+      AsukaSnackbar.alert('Corrija os erros informados').show();
     } else {
       _controller.save().then(
         (value) {
           if (value != null) {
-            asuka.hideCurrentSnackBar();
-            asuka.showSnackBar(
-              SnackBar(
-                content: const Text('Parceiro salvo com sucesso.'),
-              ),
-            );
+            AsukaSnackbar.success('Parceiro salvo com sucesso');
             Navigator.of(context).pop(value);
           } else {
-            asuka.showSnackBar(
-              SnackBar(
-                content: Text(_controller.errorMsg),
-              ),
-            );
+            AsukaSnackbar.alert(_controller.errorMsg).show();
           }
         },
       );
     }
   }
 
-  _update() async {
+  _update(BuildContext context) async {
     if (_controller.hasErrors) {
-      asuka.showSnackBar(
-        SnackBar(
-          content: const Text('Atenção: Existem erros no formulário que devem '
-              'ser corrigidos antes de efetivar a transação.'),
-        ),
-      );
+      AsukaSnackbar.alert('Corrija os erros informados').show();
     } else {
       var response = true;
       if (_controller.currentStatus == 'Inativo') {
@@ -275,27 +235,14 @@ class _PartnershipAddPageState extends State<PartnershipAddPage> {
             });
       }
       if (response == true) {
-        asuka.showSnackBar(
-          SnackBar(
-            content: const Text('Aguarde...'),
-          ),
-        );
+        AsukaSnackbar.message('Aguarde...').show();
         _controller.update().then(
           (value) {
             if (value != null) {
-              asuka.hideCurrentSnackBar();
-              asuka.showSnackBar(
-                SnackBar(
-                  content: const Text('Parceiro atualizado com sucesso.'),
-                ),
-              );
+              AsukaSnackbar.success('Parceiro atualizado com sucesso');
               Navigator.pop(context, value);
             } else {
-              asuka.showSnackBar(
-                SnackBar(
-                  content: Text(_controller.errorMsg),
-                ),
-              );
+              AsukaSnackbar.alert(_controller.errorMsg).show();
             }
           },
         );
