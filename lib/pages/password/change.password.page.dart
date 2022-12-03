029bb8ca61fd57bedb6d427988f7a789f0565879
adalbertojr-git <atajr@lsdi.ufma.br>
2022-12-02 21:23:20 -0300

    Ajustes ma tela change.password.page.dart

--- lib/pages/password/change.password.page.dart
+++ lib/pages/password/change.password.page.dart
@@ -1,122 +1,89 @@
 import 'package:asuka/asuka.dart' as asuka;
+import 'package:asuka/snackbars/asuka_snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hcslzapp/common/labels.and.hints.dart';
 import 'package:hcslzapp/components/button.dart';
 import 'package:hcslzapp/components/my.text.form.field.dart';
-import 'package:hcslzapp/components/top.bar.dart';
 import 'package:hcslzapp/controllers/change.password.controller.dart';
 import 'package:hcslzapp/models/password.dto.dart';
+import '../../components/my.appbar.dart';
+import '../../components/my.bottom.appbar.dart';
 
 const String _title = 'Alterar Senha';
 
-class ChangePasswordPage extends StatefulWidget {
+class ChangePasswordPage extends StatelessWidget {
   final PasswordDTO _passwordDTO;
+  final ChangePasswordController _controller = ChangePasswordController();
 
   ChangePasswordPage(this._passwordDTO);
 
   @override
-  _ChangePasswordPageState createState() => _ChangePasswordPageState();
-}
-
-class _ChangePasswordPageState extends State<ChangePasswordPage> {
-  ChangePasswordController _controller = ChangePasswordController();
-
-  @override
-  void initState() {
-    _controller.init();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    _controller.init();
     return Observer(
       builder: (_) {
         return Scaffold(
-          body: _widgets(_),
+          appBar: MyAppBar(_title),
+          bottomNavigationBar: MyBottomAppBar(),
+          body: _widgets(),
           floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
+              FloatingActionButtonLocation.centerDocked,
           floatingActionButton: Button(
             icon: Icons.save,
-            onClick: () => _update(),
+            onClick: () => _update(context),
           ),
         );
       },
     );
   }
 
-  _widgets(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.white30, Colors.deepOrange],
-            begin: FractionalOffset.topLeft,
-            end: FractionalOffset.bottomRight,
+  _widgets() => ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
           ),
-        ),
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              TopBar(
-                title: _title,
-              ),
-              MyTextFormField(
-                textEditingController: _controller.pswCtrl,
-                label: labelNewPsw,
-                hint: hintNewPsw,
-                icon: Icons.password,
-                inputType: TextInputType.text,
-                hidden: true,
-                onChanged: _controller.formController.changePassword,
-                errorText: _controller.validateNewPassword(),
-              ),
-              MyTextFormField(
-                textEditingController: _controller.confPswCtrl,
-                label: labelConfirmNewPsw,
-                hint: hintConfirmNewPsw,
-                icon: Icons.password,
-                inputType: TextInputType.text,
-                hidden: true,
-                onChanged: _controller.formController.changeConfPassword,
-                errorText: _controller.validateConfNewPassword(),
-              ),
-            ],
+          MyTextFormField(
+            textEditingController: _controller.pswCtrl,
+            label: labelNewPsw,
+            hint: hintNewPsw,
+            icon: Icons.password,
+            inputType: TextInputType.text,
+            hidden: true,
+            onChanged: _controller.formController.changePassword,
+            errorText: _controller.validateNewPassword(),
           ),
-        ),
+          MyTextFormField(
+            textEditingController: _controller.confPswCtrl,
+            label: labelConfirmNewPsw,
+            hint: hintConfirmNewPsw,
+            icon: Icons.password,
+            inputType: TextInputType.text,
+            hidden: true,
+            onChanged: _controller.formController.changeConfPassword,
+            errorText: _controller.validateConfNewPassword(),
+          ),
+        ],
       );
 
-  _update() {
+  _update(BuildContext context) {
     if (_controller.hasErrors) {
-      asuka.showSnackBar(
-        SnackBar(
-          content: const Text('Atenção: Existem erros no formulário que devem '
-              'ser corrigidos antes de efetivar a transação.'),
-        ),
-      );
+      AsukaSnackbar.alert('Corrija os erros informados').show();
     } else {
       _controller
           .update(
         PasswordDTO(
-          associatedId: widget._passwordDTO.associatedId,
+          associatedId: _passwordDTO.associatedId,
           aux: _controller.pswCtrl.text,
         ),
       )
           .then(
         (value) {
           if (value != null) {
-            asuka.showSnackBar(
-              SnackBar(
-                content:
-                    const Text('Alteração de senha realizada com sucesso.'),
-              ),
-            );
+            AsukaSnackbar.success('Senha alterada com sucesso').show();
             Navigator.of(context).pop();
           } else {
-            asuka.showSnackBar(
-              SnackBar(
-                content: Text(_controller.errorMsg),
-              ),
-            );
+            AsukaSnackbar.alert(_controller.errorMsg).show();
           }
         },
       );
