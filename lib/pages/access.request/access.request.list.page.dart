f4763ceb5a6cae510df995902589ea216c217313
adalbertojr-git <atajr@lsdi.ufma.br>
2023-01-05 14:55:47 -0300

    Inclusão de teste de HTTP em todos os try/catch

--- lib/pages/access.request/access.request.list.page.dart
+++ lib/pages/access.request/access.request.list.page.dart
@@ -178,7 +178,7 @@ class CheckboxWidget extends StatelessWidget {
         });
     if (response == true) {
       try {
-        controller.deleteById(loadAccessRequest(item));
+        await controller.deleteById(loadAccessRequest(item));
         AsukaSnackbar.success(SUCCESS).show();
         controller.listItems.remove(item);
       } on HttpException catch (e) {
