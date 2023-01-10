52b953188f427eb8eb8be8c27521cfa0a6b10eeb
adalbertojr-git <atajr@lsdi.ufma.br>
2023-01-10 17:40:15 -0300

    Correção de obrigatoriedade nas datas de nascimento e escudamento

--- lib/controllers/associated.update.controller.dart
+++ lib/controllers/associated.update.controller.dart
@@ -166,14 +166,16 @@ abstract class AssociatedUpdateControllerBase with Store {
   String? validateDateBirth() {
     const String _labelDatelNotValid = 'Informe uma data válida!!!';
 
-    if (!RegExp(r"^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)"
-            r"(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^"
-            r"(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|"
-            r"[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^"
-            r"(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|"
-            r"(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
-        .hasMatch(formController.dateBirth)) {
-      return _labelDatelNotValid;
+    if (formController.dateBirth.isNotEmpty) {
+      if (!RegExp(r"^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)"
+              r"(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^"
+              r"(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|"
+              r"[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^"
+              r"(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|"
+              r"(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
+          .hasMatch(formController.dateBirth)) {
+        return _labelDatelNotValid;
+      }
     }
     return null;
   }
@@ -181,14 +183,16 @@ abstract class AssociatedUpdateControllerBase with Store {
   String? validateDateShield() {
     const String _labelDatelNotValid = 'Informe uma data válida!!!';
 
-    if (!RegExp(r"^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)"
-            r"(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^"
-            r"(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|"
-            r"[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^"
-            r"(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|"
-            r"(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
-        .hasMatch(formController.dateShield)) {
-      return _labelDatelNotValid;
+    if (formController.dateShield.isNotEmpty) {
+      if (!RegExp(r"^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)"
+              r"(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^"
+              r"(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|"
+              r"[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^"
+              r"(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|"
+              r"(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
+          .hasMatch(formController.dateShield)) {
+        return _labelDatelNotValid;
+      }
     }
     return null;
   }
