7791fba18ba6799813f7bcd7a0beda8a68dfa1aa
adalbertojr-git <atajr@lsdi.ufma.br>
2023-01-22 19:54:47 -0300

    Alterado o layout da tela de notificações de aviso

--- lib/pages/notification/head.notification.list.associated.page.dart
+++ lib/pages/notification/head.notification.list.associated.page.dart
@@ -15,10 +15,12 @@ const String _pathLogoImage = 'assets/imgs/logo.png';
 
 class HeadNotificationListAssociatedPage extends StatefulWidget {
   @override
-  State<HeadNotificationListAssociatedPage> createState() => _HeadNotificationListAssociatedPageState();
+  State<HeadNotificationListAssociatedPage> createState() =>
+      _HeadNotificationListAssociatedPageState();
 }
 
-class _HeadNotificationListAssociatedPageState extends State<HeadNotificationListAssociatedPage> {
+class _HeadNotificationListAssociatedPageState
+    extends State<HeadNotificationListAssociatedPage> {
   final HeadNotificationListController _controller =
       HeadNotificationListController();
 
@@ -29,114 +31,122 @@ class _HeadNotificationListAssociatedPageState extends State<HeadNotificationLis
     }).catchError((e) {});
     super.initState();
   }
-  @override
-  Widget build(BuildContext context) => Observer(
-    builder: (_) {
-      return Scaffold(
-            appBar: MyAppBar(_title),
-            bottomNavigationBar:
-                _controller.isHidedButton ? null : MyBottomAppBar(),
-            body: FutureBuilder<List<HeadNotification>>(
-              future: _controller.future,
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                    break;
-                  case ConnectionState.waiting:
-                    return Progress();
-                  case ConnectionState.active:
-                    break;
-                  default:
-                    if (snapshot.hasError) {
-                      return CenteredMessage(
-                        title: ERROR,
-                        message: snapshot.error.toString(),
-                      );
-                    } else {
-                      if ((snapshot.data?.length)! > 0) {
-                        _controller.init();
-                        _controller.headNotifications.addAll(snapshot.data!);
-                        _controller.headNotifications.sort(
-                          (a, b) => b.datePublication.compareTo(a.datePublication),
-                        );
-                        return _widgets(context);
-                      } else
-                        return CenteredMessage(
-                          title: WARNING,
-                          message: NOTEXIST,
-                        );
-                    }
-                } //switch (snapshot.connectionState)
-                return CenteredMessage(
-                  title: ERROR,
-                  message: UNKNOWN,
-                );
-              },
-            ),
-          );
-    }
-  );
 
-  _widgets(BuildContext context) => Column(
+  @override
+  Widget build(BuildContext context) => Observer(builder: (_) {
+        return Scaffold(
+          appBar: MyAppBar(_title),
+          bottomNavigationBar:
+              _controller.isHidedButton ? null : MyBottomAppBar(),
+          body: FutureBuilder<List<HeadNotification>>(
+            future: _controller.future,
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  break;
+                case ConnectionState.waiting:
+                  return Progress();
+                case ConnectionState.active:
+                  break;
+                default:
+                  if (snapshot.hasError) {
+                    return CenteredMessage(
+                      title: ERROR,
+                      message: snapshot.error.toString(),
+                    );
+                  } else {
+                    if ((snapshot.data?.length)! > 0) {
+                      _controller.init();
+                      _controller.headNotifications.addAll(snapshot.data!);
+                      _controller.headNotifications.sort(
+                        (a, b) =>
+                            b.datePublication.compareTo(a.datePublication),
+                      );
+                      return _widgets();
+                    } else
+                      return CenteredMessage(
+                        title: WARNING,
+                        message: NOTEXIST,
+                      );
+                  }
+              } //switch (snapshot.connectionState)
+              return CenteredMessage(
+                title: ERROR,
+                message: UNKNOWN,
+              );
+            },
+          ),
+        );
+      });
+
+  _widgets() => Column(
         children: [
           SizedBox(height: 10),
-          Observer(
-            builder: (_) => Expanded(
-              child: ListView.separated(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                shrinkWrap: true,
-                itemCount: _controller.headNotifications.length,
-                itemBuilder: (_, int i) {
-                  return Column(
+          Container(
+            height: MediaQuery.of(context).size.height - 160,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: _controller.headNotifications.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width - 50,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange[300],
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Column(
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Icon(Icons.message_outlined),
-                          Text('Publicado em: ' +
-                              _controller.headNotifications[i].datePublication)
-                        ],
+                      SizedBox(
+                        height: 5,
                       ),
-                      Container(
-                        height: MediaQuery.of(context).size.height / 1.6,
-                        color: Colors.black26,
-                        child: Card(
-                          child: Column(
-                            children: <Widget>[
-                              _photo(context, i),
-                              Container(
-                                child: ListTile(
-                                  contentPadding:
-                                      EdgeInsets.fromLTRB(10, 10, 10, 0),
-                                  dense: true,
-                                  title: Text(
-                                    _controller.headNotifications[i].title +
-                                        '\n',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20.0),
-                                  ),
-                                  subtitle: Text(
-                                      _controller
-                                          .headNotifications[i].notification,
-                                      style: TextStyle(fontSize: 17.0)),
+                      _photo(index),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          width: double.infinity,
+                          child: Card(
+                            elevation: 5,
+                            child: Column(
+                              children: [
+                                Text(
+                                  _controller.headNotifications[index].title +
+                                      '\n',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18.0),
                                 ),
-                              ),
-                            ],
+                                Text(
+                                    _controller
+                                        .headNotifications[index].notification,
+                                    style: TextStyle(fontSize: 14.0)),
+                              ],
+                            ),
                           ),
                         ),
                       ),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.message_outlined),
+                            Text('Publicado em: ' +
+                                _controller
+                                    .headNotifications[index].datePublication)
+                          ],
+                        ),
+                      )
                     ],
-                  );
-                },
-                separatorBuilder: (_, int index) => const Divider(),
-              ),
+                  ),
+                );
+              },
             ),
           ),
         ],
       );
 
-  _photo(BuildContext context, int i) => Container(
+  _photo(int i) => Container(
         height: MediaQuery.of(context).size.height / 3,
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.symmetric(horizontal: 5),
