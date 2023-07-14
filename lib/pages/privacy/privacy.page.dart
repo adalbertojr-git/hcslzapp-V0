24a3266056c2905f4794c630e50a69378f8480d9
adalbertojr-git <atajr@lsdi.ufma.br>
2023-07-14 19:21:40 -0300

    Exemplo de tela para abrir Pdf do firebase

--- lib/pages/privacy/privacy.page.dart
+++ lib/pages/privacy/privacy.page.dart
@@ -1,77 +1,268 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path_provider/path_provider.dart';
 import '../../components/my.appbar.dart';
 import '../../components/my.bottom.appbar.dart';
 
 const String _title = 'Política de Privacidade';
 const String _pathLogoImage = 'assets/imgs/logo.png';
 
-class PrivacyPage extends StatelessWidget {
-  //@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MyAppBar(_title),
-      bottomNavigationBar: MyBottomAppBar(),
-      body: _widgets(),
-    );
+class PrivacyPage extends StatefulWidget {
+  @override
+  _PrivacyPageState createState() => _PrivacyPageState();
+}
+
+class _PrivacyPageState extends State<PrivacyPage> {
+  String pathPDF = "";
+  String landscapePathPdf = "";
+  String remotePDFpath = "";
+  String corruptedPathPDF = "";
+
+  @override
+  void initState() {
+    super.initState();
+/*    fromAsset('assets/corrupted.pdf', 'corrupted.pdf').then((f) {
+      setState(() {
+        corruptedPathPDF = f.path;
+      });
+    });
+    fromAsset('assets/demo-link.pdf', 'demo.pdf').then((f) {
+      setState(() {
+        pathPDF = f.path;
+      });
+    });
+    fromAsset('assets/demo-landscape.pdf', 'landscape.pdf').then((f) {
+      setState(() {
+        landscapePathPdf = f.path;
+      });
+    });*/
+
+    createFileOfPdfUrl().then((f) {
+      setState(() {
+        remotePDFpath = f.path;
+      });
+    });
   }
 
-  _widgets() => ListView(
-        children: <Widget>[
-          SizedBox(
-            height: 200.0,
-            child: Image.asset(_pathLogoImage),
-          ),
-          _buildText(),
-        ],
-      );
+  Future<File> createFileOfPdfUrl() async {
+    Completer<File> completer = Completer();
+    print("Start download file from internet!");
+    try {
+      final url =
+          "https://firebasestorage.googleapis.com/v0/b/hcslzapp.appspot.com/o/docs%2FPOL%C3%8DTICA%20DE%20PRIVACIDADE.pdf?alt=media&token=7d977af4-9463-4053-a802-e6d1bb4265cb";
+      final filename = url.substring(url.lastIndexOf("/") + 1);
+      var request = await HttpClient().getUrl(Uri.parse(url));
+      var response = await request.close();
+      var bytes = await consolidateHttpClientResponseBytes(response);
+      var dir = await getApplicationDocumentsDirectory();
+      print("Download files");
+      print("${dir.path}/$filename");
+      File file = File("${dir.path}/$filename");
 
-  Widget _buildText() {
-    return Text.rich(
-      const TextSpan(
-        text:
-            'A história do Harley Club começou em 2012 pela necessidade de se fazer '
-            'revisões em nossas máquinas, pois até hoje, São Luís não dispõe de '
-            'uma concessionária Harley Davidson®. Então, foi criado um grupo de '
-            'WhatsApp®, chamado de "Revisões HD", dessa maneira veio o mecânico '
-            'da Newroad Fortaleza (Concessionária Harley Davidson localizada em '
-            'Fortaleza-CE), onde a primeira revisão aconteceu na residência do '
-            'Harleyro Ruy Palhano.\n\n',
-        children: <TextSpan>[
-          const TextSpan(
-              text: 'Dessa maneira a turma foi crescendo, e com as revisões, '
-                  'gerou uma maior aproximação dos amigos e amantes da marca, '
-                  'promovendo uma interação e a necessidade de trocar ideias sobre o '
-                  'mundo 2 rodas e consequentemente, a realização de passeios entre '
-                  'os membros.\n\n '),
-          const TextSpan(
-            text:
-                'O grupo já estava com um bom número de participantes nos comboios '
-                'e passeios, daí, em busca do aperfeiçoamento da pilotagem e com '
-                'o objetivo de maior organização e segurança, alguns integrantes '
-                'viajaram e participaram de cursos de ‘Road Captain’ e pilotagem '
-                'módulos I, II e III em Fortaleza. Posteriormente, foi ofertado '
-                'os mesmos cursos na cidade de São Luís-MA para os interessados.\n\n',
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+
+    return completer.future;
+  }
+
+/*  Future<File> fromAsset(String asset, String filename) async {
+    // To open from assets, you can copy them to the app storage folder, and the access them "locally"
+    Completer<File> completer = Completer();
+
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/$filename");
+      var data = await rootBundle.load(asset);
+      var bytes = data.buffer.asUint8List();
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+
+    return completer.future;
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PDF View',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Plugin example app')),
+        body: Center(child: Builder(
+          builder: (BuildContext context) {
+            return Column(
+              children: <Widget>[
+                TextButton(
+                  child: Text("Open PDF"),
+                  onPressed: () {
+                    if (pathPDF.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PDFScreen(path: pathPDF),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                TextButton(
+                  child: Text("Open Landscape PDF"),
+                  onPressed: () {
+                    if (landscapePathPdf.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              PDFScreen(path: landscapePathPdf),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                TextButton(
+                  child: Text("Remote PDF"),
+                  onPressed: () {
+                    if (remotePDFpath.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PDFScreen(path: remotePDFpath),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                TextButton(
+                  child: Text("Open Corrupted PDF"),
+                  onPressed: () {
+                    if (pathPDF.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              PDFScreen(path: corruptedPathPDF),
+                        ),
+                      );
+                    }
+                  },
+                )
+              ],
+            );
+          },
+        )),
+      ),
+    );
+  }
+}
+
+class PDFScreen extends StatefulWidget {
+  final String? path;
+
+  PDFScreen({Key? key, this.path}) : super(key: key);
+
+  _PDFScreenState createState() => _PDFScreenState();
+}
+
+class _PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
+  final Completer<PDFViewController> _controller =
+      Completer<PDFViewController>();
+  int? pages = 0;
+  int? currentPage = 0;
+  bool isReady = false;
+  String errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Document"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {},
           ),
-          const TextSpan(
-            text: 'Assim, bem de mansinho, formou-se um Moto Grupo.\n\n',
-          ),
-          const TextSpan(
-            text:
-                'Já no ano de 2014 o nome passou a ser ‘Harley Club São Luís.\n\n',
-          ),
-          const TextSpan(
-              text: 'Com todo esse pessoal rodando pela cidade, encheram-se de '
-                  'coragem e esperança vários amantes sonhadores, que puderam '
-                  'tornar seus sonhos realidade, desfilando suas máquinas juntos em '
-                  'um Moto Grupo.\n\n'),
-          const TextSpan(
-              text:
-                  'Assim nasce oficialmente, no ano de 2018, o Harley Club com seu registro '
-                  'oficial, cerimônia com o escudamento de 71 membros oficiais que '
-                  'receberam seus coletes e fizeram seu juramento numa demonstração '
-                  'de amor pelo grupo. Hoje o Harley Club é um dos maiores grupo de motociclismo '
-                  'do Maranhão.\n\n'),
         ],
+      ),
+      backgroundColor: Colors.red,
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.path,
+            enableSwipe: false,
+            swipeHorizontal: true,
+            autoSpacing: false,
+            pageFling: true,
+            pageSnap: true,
+            defaultPage: currentPage!,
+            fitPolicy: FitPolicy.BOTH,
+            preventLinkNavigation: false,
+            // if set to true the link is handled in flutter
+            onRender: (_pages) {
+              setState(() {
+                pages = _pages;
+                isReady = true;
+              });
+            },
+            onError: (error) {
+              setState(() {
+                errorMessage = error.toString();
+              });
+              print(error.toString());
+            },
+            onPageError: (page, error) {
+              setState(() {
+                errorMessage = '$page: ${error.toString()}';
+              });
+              print('$page: ${error.toString()}');
+            },
+            onViewCreated: (PDFViewController pdfViewController) {
+              _controller.complete(pdfViewController);
+            },
+            onLinkHandler: (String? uri) {
+              print('goto uri: $uri');
+            },
+            onPageChanged: (int? page, int? total) {
+              print('page change: $page/$total');
+              setState(() {
+                currentPage = page;
+              });
+            },
+          ),
+          errorMessage.isEmpty
+              ? !isReady
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Container()
+              : Center(
+                  child: Text(errorMessage),
+                )
+        ],
+      ),
+      floatingActionButton: FutureBuilder<PDFViewController>(
+        future: _controller.future,
+        builder: (context, AsyncSnapshot<PDFViewController> snapshot) {
+          if (snapshot.hasData) {
+            return FloatingActionButton.extended(
+              label: Text("Go to ${pages! ~/ 2}"),
+              onPressed: () async {
+                await snapshot.data!.setPage(pages! ~/ 2);
+              },
+            );
+          }
+
+          return Container();
+        },
       ),
     );
   }
